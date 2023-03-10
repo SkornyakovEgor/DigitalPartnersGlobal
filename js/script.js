f85dd5bdbca5c59d1754c48652c1d35b26d38544
Egori <skornyakovegor231199@gmai.,com>
2023-03-10 13:07:20 +0500

    v1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let tx = document.getElementsByTagName('textarea');
+
+for (var i = 0; i < tx.length; i++) {
+    tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight - 20) + 'px;overflow-y:hidden;');
+    tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight - 10) + 'px';
+}
